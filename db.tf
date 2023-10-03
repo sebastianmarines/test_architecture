30b78a3e889c523830a7e15bf3c17af182afeab9
Sebastian Marines <sebastian0marines@gmail.com>
2023-10-03 01:26:58 -0600

    Allow DB access from EC2 subnet

--- db.tf
+++ db.tf
@@ -1,3 +1,19 @@
+resource "aws_security_group" "rds" {
+  name        = "rds"
+  description = "RDS security group"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+    security_groups = [
+      module.autoscaling_sg.security_group_id,
+      aws_security_group.bastion_host.id
+    ]
+  }
+}
+
 module "rds" {
   source  = "terraform-aws-modules/rds/aws"
   version = "6.1.1"
@@ -20,5 +36,9 @@ module "rds" {
 
   iam_database_authentication_enabled = true
 
-  subnet_ids = module.vpc.database_subnets
+  subnet_ids             = module.vpc.database_subnets
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  db_subnet_group_name   = module.vpc.database_subnet_group_name
+
+  manage_master_user_password = true
 }
